8e29b4f43d003fc320691ba9e2ad61b175196fd3
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-10-30 15:30:11 -0300

    feat: post-rendered components, env example on next added

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+// ssr option makes component only be rendered from client side
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModal'),
+  { loading: () => <p>Carregando...</p> }
+)
+
+export default function Product() {
+  const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true);
+  }
+  
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+      {isAddToCartModalVisible &&  <AddToCartModal />}
+    </div>
+  )
+}
